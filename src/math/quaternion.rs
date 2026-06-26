bc1a8e5451f53d642dbfe66875c934c12aa1bcd4
Ólafur Þór Gunnarsson <olinord@gmail.com>
2026-06-26 22:06:33 +0000

    Update quaternion.rs
    adding quaternion functions

--- src/math/quaternion.rs
+++ src/math/quaternion.rs
@@ -1,11 +1,8 @@
-use std::ops::{self, Mul, MulAssign};
+use std::ops::{Mul, MulAssign};
 
 use vulkano_macros::BufferContents;
 
-use crate::math::{
-    matrix::Mat4,
-    vector::{vec3::Vec3, vec4::Vec4},
-};
+use crate::math::vector::{vec3::Vec3, vec4::Vec4};
 
 #[derive(Debug, Clone, Copy, BufferContents)]
 #[repr(C)]
@@ -49,6 +46,95 @@ impl Quaternion {
             b: cr * sp * cy + sr * cp * sy,
             c: cr * cp * sy - sr * sp * cy,
             d: cr * cp * cy + sr * sp * sy,
+        }
+    }
+
+    pub fn normalized(quaternion: Self) -> Self {
+        let length = quaternion.a * quaternion.a
+            + quaternion.b * quaternion.b
+            + quaternion.c * quaternion.c
+            + quaternion.d * quaternion.d;
+        if length != 0.0 {
+            let length = length.sqrt();
+            Self {
+                a: quaternion.a / length,
+                b: quaternion.b / length,
+                c: quaternion.c / length,
+                d: quaternion.d / length,
+            }
+        } else {
+            Self::identity()
+        }
+    }
+
+    pub fn lerp(quat1: Self, quat2: Self, t: f32) -> Self {
+        let t = t.clamp(0.0, 1.0);
+        let a = quat1.a * (1.0 - t) + quat2.a * t;
+        let b = quat1.b * (1.0 - t) + quat2.b * t;
+        let c = quat1.c * (1.0 - t) + quat2.c * t;
+        let d = quat1.d * (1.0 - t) + quat2.d * t;
+
+        Self { a, b, c, d }
+    }
+
+    pub fn slerp(quat1: Self, quat2: Self, t: f32) -> Self {
+        let t = t.clamp(0.0, 1.0);
+        let dot = quat1.a * quat2.a + quat1.b * quat2.b + quat1.c * quat2.c + quat1.d * quat2.d;
+
+        if dot < 0.0 {
+            return Self::slerp(
+                Self {
+                    a: -quat1.a,
+                    b: -quat1.b,
+                    c: -quat1.c,
+                    d: -quat1.d,
+                },
+                quat2,
+                t,
+            );
+        }
+
+        if dot > 0.9995 {
+            return Self::lerp(quat1, quat2, t);
+        }
+
+        let theta_0 = dot.acos();
+        let theta = theta_0 * t;
+        let sin_theta = theta.sin();
+        let sin_theta_0 = theta_0.sin();
+
+        let s0 = (theta.cos() - dot * sin_theta / sin_theta_0) / sin_theta_0;
+        let s1 = sin_theta / sin_theta_0;
+
+        Self {
+            a: s0 * quat1.a + s1 * quat2.a,
+            b: s0 * quat1.b + s1 * quat2.b,
+            c: s0 * quat1.c + s1 * quat2.c,
+            d: s0 * quat1.d + s1 * quat2.d,
+        }
+    }
+
+    pub fn inverse(&self) -> Self {
+        let norm_sq = self.a * self.a + self.b * self.b + self.c * self.c + self.d * self.d;
+        if norm_sq != 0.0 {
+            let inv_norm_sq = 1.0 / norm_sq;
+            Self {
+                a: -self.a * inv_norm_sq,
+                b: -self.b * inv_norm_sq,
+                c: -self.c * inv_norm_sq,
+                d: self.d * inv_norm_sq,
+            }
+        } else {
+            Self::identity()
+        }
+    }
+
+    pub fn conjugate(&self) -> Self {
+        Self {
+            a: -self.a,
+            b: -self.b,
+            c: -self.c,
+            d: self.d,
         }
     }
 
